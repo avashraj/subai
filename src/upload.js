f3c698837a9291394253d3e0061e83b848d81fd8
avashraj <140993848+avashraj@users.noreply.github.com>
2023-11-04 17:20:21 -0700

    upload button

---
+++ src/upload.js
@@ -0,0 +1,41 @@
+import React,{useState} from 'react';
+
+function FileUpload(){
+
+    const [selectedFile, setSelectedFile] = useState();
+    const [isFilePicked, setIsFilePicked] = useState(false);
+
+    const changeHandler = (event) => {
+        const file = event.target.files[0];
+        if (file) {
+            setSelectedFile(file);
+            setIsFilePicked(true);
+        } else {
+            setSelectedFile(null);
+            setIsFilePicked(false);
+        }
+    };
+
+    const handleSubmission = () => {
+    };
+
+    return(
+        <div>
+            <input type="file" name ="file" onChange={changeHandler} />
+            {isFilePicked ? (
+                <div>
+                    <p>Filename: {selectedFile.name}</p>
+                    <p> Filetype: {selectedFile.type}</p>
+                </div>
+            ) : (
+                <p>Select a file to show details</p>
+            )}
+            
+            <div>
+                <button onClick={handleSubmission}></button>
+            </div>
+        </div>
+    )
+}
+
+export default FileUpload;
