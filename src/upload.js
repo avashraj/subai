8ba7fa838861718efbffe23a0ef4f189a3748641
Leo Rosales <leorosal@usc.edu>
2023-11-05 02:03:07 -0800

    deleted duplicate line

--- src/upload.js
+++ src/upload.js
@@ -18,49 +18,49 @@ function FileUpload() {
 
 
 
-    const handleSubmission = () => {
-        const handleSubmission = async () => {
-            const formData = new FormData();
-            formData.append('file', selectedFile);
 
-            try {
-                const endpoint = "http://127.0.0.1:8000/upload_file"
-                const response = await fetch(endpoint, {
-                    method: "POST",
-                    body: formData,
-                    mode: 'cors',
-                });
+    const handleSubmission = async () => {
+        const formData = new FormData();
+        formData.append('file', selectedFile);
 
-                if (response.ok) {
-                    console.log("Successful File Upload");
-                } else {
-                    console.error("Upload failed");
-                }
-            } catch (error) {
-                console.error(error);
+        try {
+            const endpoint = "http://127.0.0.1:8000/upload_file"
+            const response = await fetch(endpoint, {
+                method: "POST",
+                body: formData,
+                mode: 'cors',
+            });
+
+            if (response.ok) {
+                console.log("Successful File Upload");
+            } else {
+                console.error("Upload failed");
             }
-        };
-
+        } catch (error) {
+            console.error(error);
+        }
     };
 
-    return (
-        <div className="file-upload-container">
-            <label htmlFor="file-upload" className="file-input-button">Choose File</label>
-            <input id="file-upload" type="file" name="file" onChange={changeHandler} />
-            {isFilePicked ? (
-                <div className="file-details">
-                    <p>Filename: {selectedFile.name}</p>
-                    <p> Filetype: {selectedFile.type}</p>
-                </div>
-            ) : (
-                <p>Select a file to show details</p>
-            )}
+};
 
-            <div>
-                <button onClick={handleSubmission}>Upload</button>
+return (
+    <div className="file-upload-container">
+        <label htmlFor="file-upload" className="file-input-button">Choose File</label>
+        <input id="file-upload" type="file" name="file" onChange={changeHandler} />
+        {isFilePicked ? (
+            <div className="file-details">
+                <p>Filename: {selectedFile.name}</p>
+                <p> Filetype: {selectedFile.type}</p>
             </div>
+        ) : (
+            <p>Select a file to show details</p>
+        )}
+
+        <div>
+            <button onClick={handleSubmission}>Upload</button>
         </div>
-    )
+    </div>
+)
 }
 
 export default FileUpload;
