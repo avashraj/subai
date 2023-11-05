319b6437e96c4a66a287ee8740962b833ee25049
avashraj <140993848+avashraj@users.noreply.github.com>
2023-11-04 17:21:37 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-
 import './App.css';
-
+import FileUpload from './upload.js';
 function App() {
   return (
     <div className="App">
-      
+      <h1>subAI</h1>
+      <p>Please select a video or audio file from your computer</p>
+      <FileUpload />
     </div>
   );
 }
