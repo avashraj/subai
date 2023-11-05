6eb449e1c0986b8707210360371a3ed1a6f25a81
avashraj <140993848+avashraj@users.noreply.github.com>
2023-11-04 18:52:13 -0700

    text and button in app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
 import './App.css';
 import FileUpload from './upload.js';
+import React,{useState} from 'react';
+
+
 function App() {
+  
+  const[textInput, setTextInput] = useState('');
+
+  const handleInputChange = (event) => {
+    setTextInput(event.target.value);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('Submitted text:', textInput)
+  }
+  
   return (
     <div className="App">
       <h1>subAI</h1>
       <p>Please select a video or audio file from your computer</p>
       <FileUpload />
+      <form onSubmit={handleSubmit}>
+      <div>
+        <p>Enter any questions: </p>
+        <input 
+        type="text"
+        value={textInput}
+        onChange = {handleInputChange}
+        />
+      </div>
+      <div>
+        <button type="submit">Submit</button>
+      </div>
+      </form>
+      <div>
+        <h2>Practice Problem</h2> 
+      </div>
     </div>
   );
 }
