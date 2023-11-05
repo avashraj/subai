df36c8160b17738219b37504683c69c04941f0a7
avashraj <140993848+avashraj@users.noreply.github.com>
2023-11-05 02:54:57 -0800

    text.js w/ post

--- src/text.js
+++ src/text.js
@@ -3,14 +3,19 @@ import React, { useState, useEffect } from 'react';
 function ScrollableBox() {
   const [text, setText] = useState('');
   const [isLoading, setIsLoading] = useState(false);
-  const [textInput, setTextInput] = useState('');
-  const [response, setResponse] = useState('');
 
   const fetchDataFromApi = () => {
     setIsLoading(true);
 
     // Fetch data from your API
-    fetch('http://localhost:8000/answer')
+    fetch('http://localhost:8000/answer', {
+      method: 'POST',  // Change the method to POST
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({}),  
+      mode: 'cors',
+    })
       .then((response) => response.json())
       .then((data) => {
         setText(data.text);
@@ -21,29 +26,6 @@ function ScrollableBox() {
       .finally(() => {
         setIsLoading(false);
       });
-  };
-
-  const handleTextSend = async () => {
-    try {
-      const data = { text: textInput }; // Data to send
-      const response = await fetch('http://localhost:8000/answer', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
-        mode:'cors',
-      });
-
-      if (response.ok) {
-        const result = await response.json();
-        setResponse(result.text);
-      } else {
-        setResponse('Failed to send text');
-      }
-    } catch (error) {
-      console.error('Error sending text:', error);
-    }
   };
 
   useEffect(() => {
@@ -58,13 +40,6 @@ function ScrollableBox() {
       <div style={{ width: '300px', height: '200px', overflow: 'auto' }}>
         <p>{text}</p>
       </div>
-      <input
-        type="text"
-        value={textInput}
-        onChange={(e) => setTextInput(e.target.value)}
-      />
-      <button onClick={handleTextSend}>Send Text</button>
-      <div>{response}</div>
     </div>
   );
 }
