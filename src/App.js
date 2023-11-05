2aca44bd51c7e3af019a189e68e9dd2bf1b23afd
Leo Rosales <leorosal@usc.edu>
2023-11-05 01:44:32 -0800

    Improved styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import FileUpload from './upload.js';
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 
 
 function App() {
-  
-  const[textInput, setTextInput] = useState('');
+
+  const [textInput, setTextInput] = useState('');
 
   const handleInputChange = (event) => {
     setTextInput(event.target.value);
@@ -15,27 +15,29 @@ function App() {
     event.preventDefault();
     console.log('Submitted text:', textInput)
   }
-  
+
   return (
     <div className="App">
-      <h1>subAI</h1>
+      <container className="App-header">
+        <h1>subAI</h1>
+      </container>
       <p>Please select a video or audio file from your computer</p>
       <FileUpload />
       <form onSubmit={handleSubmit}>
-      <div>
-        <p>Enter any questions: </p>
-        <input 
-        type="text"
-        value={textInput}
-        onChange = {handleInputChange}
-        />
-      </div>
-      <div>
-        <button type="submit">Submit</button>
-      </div>
+        <div>
+          <p>Enter any questions: </p>
+          <input
+            type="text"
+            value={textInput}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div>
+          <button type="submit">Submit</button>
+        </div>
       </form>
       <div>
-        <button>Practice Problem</button> 
+        <button>Generate Practice Problem</button>
       </div>
     </div>
   );
--- src/upload.js
+++ src/upload.js
@@ -1,6 +1,6 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 
-function FileUpload(){
+function FileUpload() {
 
     const [selectedFile, setSelectedFile] = useState();
     const [isFilePicked, setIsFilePicked] = useState(false);
@@ -16,46 +16,24 @@ function FileUpload(){
         }
     };
 
-    const handleSubmission = async () => {
-        const formData = new FormData();
-        formData.append('file', selectedFile);
-      
-        try {
-          const endpoint = "http://127.0.0.1:8000/upload_file"
-          const response = await fetch(endpoint, {
-            method: "POST",
-            body: formData,
-            mode: 'cors',
-          });
-      
-          if (response.ok) {
-            console.log("Successful File Upload");
-          } else {
-            console.error("Upload failed");
-          }
-        } catch (error) {
-          console.error(error);
-        }
-      };
-      
-            
+    const handleSubmission = () => {
+    };
 
-    
-
-    return(
-        <div>
-            <input type="file" name ="file" onChange={changeHandler} />
+    return (
+        <div className="file-upload-container">
+            <label htmlFor="file-upload" className="file-input-button">Choose File</label>
+            <input id="file-upload" type="file" name="file" onChange={changeHandler} />
             {isFilePicked ? (
-                <div>
+                <div className="file-details">
                     <p>Filename: {selectedFile.name}</p>
                     <p> Filetype: {selectedFile.type}</p>
                 </div>
             ) : (
                 <p>Select a file to show details</p>
             )}
-            
+
             <div>
-            <button onClick={handleSubmission}>Upload</button>
+                <button onClick={handleSubmission}>Upload</button>
             </div>
         </div>
     )
