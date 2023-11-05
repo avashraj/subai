d4bfd7322c8d30f3422d049d311ca4f68fa433c6
avashraj <140993848+avashraj@users.noreply.github.com>
2023-11-05 02:43:33 -0800

    Update text.js

--- src/text.js
+++ src/text.js
@@ -1,8 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function ScrollableBox() {
   const [text, setText] = useState('');
   const [isLoading, setIsLoading] = useState(false);
+  const [textInput, setTextInput] = useState('');
+  const [response, setResponse] = useState('');
 
   const fetchDataFromApi = () => {
     setIsLoading(true);
@@ -11,7 +13,7 @@ function ScrollableBox() {
     fetch('http://localhost:8000/answer')
       .then((response) => response.json())
       .then((data) => {
-        setText(data.text); 
+        setText(data.text);
       })
       .catch((error) => {
         console.error('Error fetching data: ', error);
@@ -21,6 +23,33 @@ function ScrollableBox() {
       });
   };
 
+  const handleTextSend = async () => {
+    try {
+      const data = { text: textInput }; // Data to send
+      const response = await fetch('http://localhost:8000/answer', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+        mode:'cors',
+      });
+
+      if (response.ok) {
+        const result = await response.json();
+        setResponse(result.text);
+      } else {
+        setResponse('Failed to send text');
+      }
+    } catch (error) {
+      console.error('Error sending text:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchDataFromApi();
+  }, []);
+
   return (
     <div>
       <button onClick={fetchDataFromApi} disabled={isLoading}>
@@ -29,6 +58,13 @@ function ScrollableBox() {
       <div style={{ width: '300px', height: '200px', overflow: 'auto' }}>
         <p>{text}</p>
       </div>
+      <input
+        type="text"
+        value={textInput}
+        onChange={(e) => setTextInput(e.target.value)}
+      />
+      <button onClick={handleTextSend}>Send Text</button>
+      <div>{response}</div>
     </div>
   );
 }
