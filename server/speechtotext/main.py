a3f7545249e02028e23db07f7b28379474b8b736
Leo Rosales <leorosal@usc.edu>
2023-11-04 20:07:25 -0700

    Edited variable name

--- server/speechtotext/main.py
+++ server/speechtotext/main.py
@@ -3,6 +3,6 @@ import whisper
 def transcribe_audio_to_text(audio_file_path):
 
     model = whisper.load_model("base")
-    result = model.transcribe( audio_path, fp16=False)
+    result = model.transcribe( audio_file_path, fp16=False)
     return result["text"]
 
